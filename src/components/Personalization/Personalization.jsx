101680c929781d62cf17677d466acee79e73ae8c
Tomer Bourstein <88827223+tomerbourstein@users.noreply.github.com>
2023-03-19 20:14:37 +0200

    added button to preferences

--- src/components/Personalization/Personalization.jsx
+++ src/components/Personalization/Personalization.jsx
@@ -2,10 +2,11 @@ import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchNewsCategoriesData } from "../../store/news-requests";
 import { apiActions } from "../../store/api-slice";
-
-import Menu from "@mui/material/Menu";
+import Popover from "@mui/material/Popover";
+import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
 import Chip from "@mui/material/Chip";
+import Button from "@mui/material/Button";
 
 const clickedCategories = ["Sports", "Music", "Cryptocurrency", "Fashion"];
 
@@ -13,7 +14,9 @@ const Personalization = (props) => {
   const newsCategories = useSelector((state) => state.api.categories);
   const dispatch = useDispatch();
 
-  const [clickedCategory, setClickedCategory] = useState([...clickedCategories]);
+  const [clickedCategory, setClickedCategory] = useState([
+    ...clickedCategories,
+  ]);
 
   const addCategoryHandler = (categoryName) => {
     if (clickedCategory.length >= 4) return;
@@ -26,12 +29,17 @@ const Personalization = (props) => {
     const newArray = clickedCategory.filter((item) => item != categoryName);
     setClickedCategory(newArray);
   };
+  const setPrefHandler = (clickedCategories) => {
+    console.log(clickedCategories);
+  };
+
   useEffect(() => {
     dispatch(fetchNewsCategoriesData());
   }, [dispatch]);
 
   return (
-    <Menu
+    <Popover
+      sx={{ pl: 100 }}
       anchorEl={props.anchorPersonal}
       id="account-selections"
       open={props.openPersonal}
@@ -75,7 +83,17 @@ const Personalization = (props) => {
           sx={{ m: 1 }}
         ></Chip>
       ))}
-    </Menu>
+
+      <Divider />
+      <Box sx={{ p: 1, display: "flex", justifyContent: "flex-end" }}>
+        <Button
+          variant="contained"
+          onClick={() => setPrefHandler(clickedCategories)}
+        >
+          Set preferences
+        </Button>
+      </Box>
+    </Popover>
   );
 };
 
